848c1d5ab03107d6d991e16bf5fe8ee8df4c82a1
Dmitry-kl <i6169696@gmail.com>
2023-01-22 23:44:56 +0300

    add mongoose adn basic schema

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,10 +1,34 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config({
     path: './.env'
 })
-
+const DB = process.env.DATABAS.replace('<PASSWORD>', process.env.DATABSE_PASS)
 const app = require('./app')
 
+mongoose.set('strictQuery', false);
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+})
+    .then(()=> console.log('DB connected'))
+
+const tourSchema = new mongoose.Schema({ //Описываем схему нашей модули
+    name: {
+        type: String, // Тип поля
+        require: [true, 'A Tour must have a name'], // обязательно для заполнения, в массиве пишем что выводим при ошибке
+        unique: true //Поле name должно быть уникальным
+    },
+    price: {
+        type: String,
+        require: [true, 'A Tour must have a price']
+    },
+    rating: {
+        type: Number,
+        default: 4.5 // Значение по умолчанию
+    }
+})
+
+const Tour = mongoose.model('Tour', tourSchema) //Создаем модель на основе схемы
 
 const port = process.env.port;
 
